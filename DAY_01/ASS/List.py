0d88db86cc510fda28e5a85b1617206f33039362
Varma <narishettinagaraju26@gmail.com>
2024-12-17 10:47:16 +0530

    update

---
+++ DAY_01/ASS/List.py
@@ -0,0 +1,21 @@
+list1=[True,False,'Python',5,5.0]
+
+# List is ordered
+print(list1[0])
+print(list1[2])
+
+# List is mutable
+list1[0]='a'
+print(list1)
+
+#append 
+''' in append we need to include value append(value)'''
+
+list1.append(2)
+print(list1)
+
+# insert at index
+''' Insert method accepts two paramers index and input'''
+list1.insert(2,'two')
+print(list1)
+
